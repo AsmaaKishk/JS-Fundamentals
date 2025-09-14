90eb4c3ee98c0db2fd0b9dd21e90792265e51b74
AsmaaKishk <92226148+AsmaaKishk@users.noreply.github.com>
2025-09-14 15:01:15 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,17 @@
 
 
-const x = "C is fun"
-if(x !== is(NAN)){
-  console.log("Missing number of occurrences")
+// const x = "C is fun"
+// if(x !== is(NAN)){
+//   console.log("Missing number of occurrences")
+// }
+
+const arg = process.argv[2];
+const x = parseInt(arg, 10);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else if (x > 0) {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
 }
