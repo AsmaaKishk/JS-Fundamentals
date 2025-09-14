e98b09bf902b9d546de2aa7df7e31d07139a2fac
AsmaaKishk <92226148+AsmaaKishk@users.noreply.github.com>
2025-09-14 14:49:19 +0300

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,26 +1,8 @@
+const arg = process.argv[2];           // أول argument بعد اسم الملف
+const num = parseInt(arg, 10);         // تحويله لرقم صحيح
 
-}
-const myArg = process.argv[2];       // أول argument
-const number = parseInt(myArg, 10);  // تحويله لرقم صحيح
-
-if (isNaN(number)) {
-    console.log("Not a number");     // لو مش رقم
+if (isNaN(num)) {
+  console.log("Not a number");         // لو مش رقم
 } else {
-    console.log(`My number: ${number}`);  // لو رقم
+  console.log(`My number: ${num}`);    // لو رقم
 }
-
-node 5-to_integer.js
-# Not a number
-
-node 5-to_integer.js 89
-# My number: 89
-
-node 5-to_integer.js "89"
-# My number: 89
-
-node 5-to_integer.js 89.89
-# My number: 89
-
-node 5-to_integer.js School
-# Not a number
-
