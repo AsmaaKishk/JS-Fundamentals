45900d1054e884a7076be305ab66c67a9b53eb54
AsmaaKishk <92226148+AsmaaKishk@users.noreply.github.com>
2025-09-14 14:31:00 +0300

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,5 +1,5 @@
 
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+const name1 = process.name[2];
+const name2 = process.name[3];
 
-console.log(`${arg1} is ${arg2}`);
+console.log(`${name1} is ${name2}`);
